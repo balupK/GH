65d2ce7d0a1706dbaa5f9b34be5f7b7f520d438c
Balakrishna P <balakrishna.puttaparthini@techwave.net>
2018-11-30 19:13:27 +0530

    Initial commit

---
+++ ng-pi-admin-master/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { PagesModule } from './pages/pages.module';
+import { routing } from './app.routing';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    PagesModule,
+    routing,
+    HttpClientModule
+  ],
+  declarations: [
+    AppComponent,
+  ],
+  providers: [
+    HttpClient,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

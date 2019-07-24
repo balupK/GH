114b500db4c31e4df623998d8bb6753a647c426c
pbalu369@gmail.com <balakrishna.puttaparthini@techwave.net>
2019-07-24 15:50:00 +0530

    JWt sample

---
+++ SpringBootJwtAuthentication/src/main/java/com/gh/jwtauthentication/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.gh.jwtauthentication.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.gh.jwtauthentication.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+    @Query("select u from User u where u.email = :email or u.username = :username")
+    Optional<User> findByEmailOrUsername(@Param("email") String email,
+                                         @Param("username") String username);
+}//findByLastnameOrFirstname
